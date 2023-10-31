88e455a40dc62acd924dda8d48004c46a7b03b6e
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-10-31 19:30:29 +0600

    Setup default font styling and added some basic color variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 const App = () => {
   return (
-    <div>
-      <p className="grid min-h-[100dvh] place-items-center text-4xl font-bold">
-        Hello world
-      </p>
+    <div className="grid min-h-[100dvh] items-center">
+      <section>
+        <div className="container">
+          <div className="gallery rounded-xl border bg-gray-100 shadow-md">
+            <div className="header flex items-center p-4">
+              <div>
+                <h1>Marked</h1>
+                <h2>Marked</h2>
+                <h3>Marked</h3>
+                <h4>Marked</h4>
+                <h5>Marked</h5>
+                <h6>Marked</h6>
+                <p>Marked</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,19 @@ export default {
   theme: {
     extend: {
       container: {
-        centered: true,
+        center: true,
         padding: '2rem'
       },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        background: 'var(--background-color)',
+        card: 'var(--card-color)',
+        body: 'var(--body-color)',
+        accent: 'var(--accent-color)',
+        link: 'var(--link-color)',
+        'link-hover': 'var(--link-hover-color)',
       }
     },
   },
